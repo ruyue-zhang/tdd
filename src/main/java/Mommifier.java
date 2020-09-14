55da05f4b5867d56da96851a36c4f3bc779d41e4
lpdft <772346730@qq.com>
2020-09-14 22:17:51 +0800

    [ruyue] feat:implement throw inputException when input is null

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,8 @@
+public class Mommifier {
+    public String addMommy(String input) throws InputException {
+        if(input == null) {
+            throw new InputException();
+        }
+        return null;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MommifierTest {
+    @Test
+    void should_throw_input_exception_when_input_null() {
+        assertThrows(InputException.class, () -> {
+            Mommifier mommifier = new Mommifier();
+            mommifier.addMommy(null);
+        });
+    }
 }
